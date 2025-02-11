2c3b7119b85698a85f47ff13ba8030e4bedd5e62
shubhamk099 <shubhamkkumar9797@gmail.com>
2025-02-11 10:21:07 +0530

    Inject beans using @Autowired on class fields

---
+++ example13/src/main/java/com/example/beans/Person.java
@@ -0,0 +1,37 @@
+package com.example.beans;
+
+import jakarta.annotation.PostConstruct;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+public class Person {
+    private String name;
+    private Vehicle vehicle;
+
+    public Person() {
+        System.out.println("Person bean created by Spring");
+    }
+
+    @PostConstruct
+    public void initialize() {
+        this.name = "Lucy";
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Vehicle getVehicle() {
+        return vehicle;
+    }
+
+    @Autowired
+    public  void setVehicle(Vehicle vehicle) {
+        this.vehicle = vehicle;
+    }
+}
