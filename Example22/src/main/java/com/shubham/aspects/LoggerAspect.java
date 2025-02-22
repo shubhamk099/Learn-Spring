117e8873a892f6ee98ae8d7d21db64291abacdbc
shubhamk099 <shubhamkkumar9797@gmail.com>
2025-02-22 08:22:55 +0530

    Added @Order in Aspects

--- Example22/src/main/java/com/shubham/aspects/LoggerAspect.java
+++ Example22/src/main/java/com/shubham/aspects/LoggerAspect.java
@@ -3,6 +3,7 @@ package com.shubham.aspects;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
+import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
 
 import java.time.Duration;
@@ -11,6 +12,7 @@ import java.util.logging.Logger;
 
 @Aspect
 @Component
+@Order(2)
 public class LoggerAspect {
     private final Logger logger = Logger.getLogger(LoggerAspect.class.getName());
 
--- Example22/src/main/java/com/shubham/aspects/VehicleStartCheckAspect.java
+++ Example22/src/main/java/com/shubham/aspects/VehicleStartCheckAspect.java
@@ -4,12 +4,14 @@ package com.shubham.aspects;
 import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
+import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
 
 import java.util.logging.Logger;
 
 @Aspect
 @Component
+@Order(1)
 public class VehicleStartCheckAspect {
     private final Logger logger = Logger.getLogger(VehicleStartCheckAspect.class.getName());
 
